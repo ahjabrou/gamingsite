b988e23c44d7d04fdd2a8104202e4f71d22a6cb1
ahjabrou <ahjagraceermadinebrou@gmail.com>
2023-03-19 23:34:47 +0000

    change db value

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ var con = mysql.createConnection({
   user     : process.env.DB_USER,
   password : process.env.DB_PASSWORD,
   database : process.env.DATABASE,
-  port     : process.env.DB_PORT 
+  // port     : process.env.DB_PORT 
 
   
 
